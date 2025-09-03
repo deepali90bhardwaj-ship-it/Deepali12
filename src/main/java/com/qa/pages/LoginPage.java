9907e3ba142e86c45a4848a6593314cf645d4bc1
abcd <dee@gmail.com>
2025-09-03 16:25:10 +0530

    added search page feature code

--- src/main/java/com/qa/pages/LoginPage.java
+++ src/main/java/com/qa/pages/LoginPage.java
@@ -1,5 +1,14 @@
 package com.qa.pages;
 
 public class LoginPage {
+	
+	public void loginPage()
+	{
+		System.out.println("Login Page");
+	}
+	public void loginSetMethod()
+	{
+		System.out.println("Login Page module");
+	}
 
 }
